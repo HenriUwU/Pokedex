e48eaeadd77a2610befb393319975660996bb8a2
Henri Sebille <henrisebille69@gmail.com>
2024-10-21 17:06:21 +0200

    setup initial spring boot app, and setup application.properties with password hardcoded

---
+++ src/test/java/io/tutorial/spring/Pokedex/PokedexApplicationTests.java
@@ -0,0 +1,13 @@
+package io.tutorial.spring.Pokedex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokedexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
