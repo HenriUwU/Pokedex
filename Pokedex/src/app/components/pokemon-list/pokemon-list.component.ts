a5dacf916ed6ae781f0306cd6250ffed0332481d
Henri Sebille <henrisebille69@gmail.com>
2024-10-23 14:11:47 +0200

    some changes to front

--- Pokedex/src/app/components/pokemon-list/pokemon-list.component.ts
+++ Pokedex/src/app/components/pokemon-list/pokemon-list.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { PokemonCardComponent } from "../pokemon-card/pokemon-card.component";
 import { RouterLink } from '@angular/router';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-pokemon-list',
   standalone: true,
-  imports: [PokemonCardComponent, RouterLink],
+  imports: [PokemonCardComponent, RouterLink, NgClass],
   templateUrl: './pokemon-list.component.html',
   styleUrl: './pokemon-list.component.scss'
 })
 export class PokemonListComponent {
+  isModalOpen = false;
+  
+  addPokemonModal() {
+    
+  }
 
+  closeModal() {
+    this.isModalOpen = false;
+  }
 }
